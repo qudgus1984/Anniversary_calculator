cb30ebc281d5f7e45baaab25183bdc234abc2df8
ByeongHyeon <qudgus1984@naver.com>
2022-07-14 12:33:05 +0900

    새싹 2주차 4일 (목) 오픈소스 / DateFormat 실습

--- Anniversary_calculator/ViewController.swift
+++ Anniversary_calculator/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //DateFormatter : 알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:ss)
+        let format = DateFormatter()
+        format.dateFormat = "M월 d일 yy년"
+        
+        let result = format.string(from: Date())
+        print(result, Date())
+        
+        let word = "3월 2일, 19년"
+        let dateResult = format.date(from: word)
+        
+        print(dateResult)
     }
 
 
